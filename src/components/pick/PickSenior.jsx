72cf433440dcebca60860aa551d670227d2aed30
icelemon-czy <71572625+icelemon-czy@users.noreply.github.com>
2022-11-27 21:07:11 -0500

    update

--- src/components/pick/PickSenior.jsx
+++ src/components/pick/PickSenior.jsx
@@ -1,10 +1,55 @@
 import "./picksenior.scss"
 import {useState} from "react";
 import axios from "axios";
+import {useMutation, useQueryClient} from "@tanstack/react-query";
+import {makeRequest} from "../../axios";
 
 const SeniorPick =({setMatchSenior,setOpenPickSenior,setOpenSuccessPage,seniors,matchInfo})=>{
     const[selectedSenior,setSenior] = useState(null);
     const [err,setErr] = useState(null);
+
+    // Use Query client
+    const queryClient = useQueryClient();
+
+    // Add Mutations - add timeslot
+    const mutation = useMutation(
+        (newTimeSlot) => {
+            return makeRequest.post("/time/parent", newTimeSlot);
+        },
+        {
+            onSuccess: () => {
+                // Invalidate and refetch
+                queryClient.invalidateQueries(['time']);
+            },
+        }
+    );
+
+    // Delete Mutations -delete timeslot
+    const delMutation = useMutation(
+        (timeSlotId) => {
+            return makeRequest.delete("/time/parent", {data:{id:timeSlotId}});
+        },
+        {
+            onSuccess: () => {
+                // Invalidate and refetch
+                queryClient.invalidateQueries(['time']);
+            },
+        }
+    );
+
+    // Match Mutation - add match
+    const matchMutation = useMutation(
+        (newMatch) => {
+            return makeRequest.post("/match", newMatch);
+        },
+        {
+            onSuccess: () => {
+                // Invalidate and refetch
+                queryClient.invalidateQueries(['pmatch']);
+            },
+        }
+    );
+
     const handleChange = (e)=>{
         setSenior(e.target.value);
         for(var i = 0;i<seniors.length;i++){
@@ -37,28 +82,160 @@ const SeniorPick =({setMatchSenior,setOpenPickSenior,setOpenSuccessPage,seniors,
                     return res.data;
                 }
             );
+
             let tid = data[0].id;
             let type = data[0].type;
             let start = data[0].start;
             let end = data[0].end;
-            // Delete the time slot
+            // Delete the original time slot
+            delMutation.mutate(tid);
+            // Add to the Match
+            matchMutation.mutate({
+                sid: selectedSenior,
+                pid: matchInfo.pet,
+                cid :matchInfo.child,
+                year: matchInfo.year,
+                month: matchInfo.month,
+                day: matchInfo.day,
+                start: matchInfo.start,
+                end: matchInfo.end+1,
+            });
 
+
+            // Pet   (TimeSlot 0 or 1)
+            // Child (TimeSlot 0 or 2)
             // Then add new timeslot
+            // Case 1 Pet is null
+            if(matchInfo.pet == null){
+                if(type ==0){
+                    mutation.mutate({
+                        sid: selectedSenior,
+                        type: 1,
+                        year: matchInfo.year,
+                        month: matchInfo.month,
+                        day: matchInfo.day,
+                        start: start,
+                        end: end,
+                    });
+                }
+                if(matchInfo.start == start){
+                    // Nothing to do
+                }else{
+                    mutation.mutate({
+                        sid: selectedSenior,
+                        type: 2,
+                        year: matchInfo.year,
+                        month: matchInfo.month,
+                        day: matchInfo.day,
+                        start: matchInfo.start,
+                        end: start,
+                    });
+                }
 
-            // setSeniors(data);
-            // if(data.length > 0){
-            //     setOpenPickSenior(true);
-            //     setSeniorMatch(false);
-            // }else{
-            //     setErr("Can not find available seniors");
-            // }
+                if(matchInfo.end+1 == end){
+                    // Nothing to do
+                }else{
+                    // end > match end+1
+                    mutation.mutate({
+                        sid: selectedSenior,
+                        type: 2,
+                        year: matchInfo.year,
+                        month: matchInfo.month,
+                        day: matchInfo.day,
+                        start: matchInfo.end+1,
+                        end: end,
+                    });
+                }
+
+                setOpenPickSenior(false);
+                setOpenSuccessPage(true);
+                return;
+            }
+
+            // Case 2 Child is null
+            if(matchInfo.child == null){
+                if(type == 0){
+                    mutation.mutate({
+                        sid: selectedSenior,
+                        type: 2,
+                        year: matchInfo.year,
+                        month: matchInfo.month,
+                        day: matchInfo.day,
+                        start: start,
+                        end: end,
+                    });
+                }
+                if(matchInfo.start == start){
+                    // Nothing to do
+                }else{
+                    mutation.mutate({
+                        sid: selectedSenior,
+                        type: 1,
+                        year: matchInfo.year,
+                        month: matchInfo.month,
+                        day: matchInfo.day,
+                        start: matchInfo.start,
+                        end: start,
+                    });
+                }
+
+                if(matchInfo.end+1 == end){
+                    // Nothing to do
+                }else{
+                    // end > match end+1
+                    mutation.mutate({
+                        sid: selectedSenior,
+                        type: 1,
+                        year: matchInfo.year,
+                        month: matchInfo.month,
+                        day: matchInfo.day,
+                        start: matchInfo.end+1,
+                        end: end,
+                    });
+                }
+                setOpenPickSenior(false);
+                setOpenSuccessPage(true);
+                return;
+            }
+
+            // Case 3 Child and pet both are picked
+            if(matchInfo.start == start){
+                // Nothing to do
+            }else{
+                mutation.mutate({
+                    sid: selectedSenior,
+                    type: 0,
+                    year: matchInfo.year,
+                    month: matchInfo.month,
+                    day: matchInfo.day,
+                    start: matchInfo.start,
+                    end: start,
+                });
+            }
+
+            if(matchInfo.end+1 == end){
+                // Nothing to do
+            }else{
+                // end > match end+1
+                mutation.mutate({
+                    sid: selectedSenior,
+                    type: 0,
+                    year: matchInfo.year,
+                    month: matchInfo.month,
+                    day: matchInfo.day,
+                    start: matchInfo.end+1,
+                    end: end,
+                });
+            }
+
+            setOpenPickSenior(false);
+            setOpenSuccessPage(true);
+
         }catch (err){
             setErr(err);
-            return;
         }
-        setOpenPickSenior(false);
-        setOpenSuccessPage(true);
     }
+
     return (
         <div className={"pick"}>
             <div className="wrapper">
