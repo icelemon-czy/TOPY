d6c036c1eebd11abb7564797944cdfcecc5ca86f
icelemon-czy <czy1121098027@gmail.com>
2022-12-10 11:45:15 -0500

    Delete routes directory

--- routes/match.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import {getMatches, getPMatches, deleteMatch, addMatch} from "../controllers/match.js";
-
-const router = express.Router();
-router.get("/senior/",getMatches);
-router.get("/parent/",getPMatches);
-router.delete("/parent/",deleteMatch);
-router.post("/",addMatch);
-export default router;
--- routes/pet.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import {getPets,addPet} from "../controllers/pet.js";
-
-const router = express.Router();
-router.get("/",getPets);
-router.post("/",addPet);
-
-export default router;
